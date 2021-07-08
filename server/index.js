f2dde5ba0a32c18f6d980d54484fc9722beebeee
dbarraca <daniel.barraca@gmail.com>
2021-07-08 09:41:52 -0700

    Setup production deploy

--- server/index.js
+++ server/index.js
@@ -6,8 +6,24 @@ const path = require('path');
 
 require('dotenv').config();
 
+// Databse config
+if (process.env.NODE_ENV === 'production') {
+    let db = process.env.MONGODB_URI;
+    // Connect to Mongo
+    mongoose.connect(db)
+    .then(() => console.log('MongoDB Connected...'))
+    .catch(err => console.log(err));
+}
+else {
+    let db = require('./config/keys').mongoURI;
+    // Connect to Mongo
+    mongoose.connect(db)
+    .then(() => console.log('MongoDB Connected...'))
+    .catch(err => console.log(err));
+}
+
 const app  = express();
-app.use(cors());
+// app.use(cors());
 app.use(bodyParser.json());
 app.use(express.json());
 
@@ -16,20 +32,6 @@ app.use((req, res, next) => {
     res.header("Access-Control-Allow-Origin", "*");
     res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
     next();
-});
-
-// Mongo
-const uri = process.env.ATLAS_URI;
-mongoose.connect(
-    uri,
-    {
-        useUnifiedTopology: true,
-        useNewUrlParser: true, 
-        useCreateIndex: true
-    }
-)
-.catch(err => {
-    console.log(`DB Connection Error: ${err.message}`);
 });
 
 const connection = mongoose.connection;
@@ -41,9 +43,22 @@ connection.once('open', () => {
 var sitesRouter = require('./routes/sites');
 app.use('/sites', sitesRouter);
 
-app.get('/',function(req, res) {
-    res.send("Site Builder Web App");
-});
+// If in production, then use static frontend build files.
+if (process.env.NODE_ENV === 'production') {
+    // Serve static folder
+    app.use(express.static('../web/build'));
+
+    // Handle React routing, return all requests to React app
+    app.get('*', function(req, res) {
+        res.sendFile(path.resolve(__dirname, '../client/build', 'index.html'));
+    });
+}
+else {
+    app.get('/', function(req, res) {
+        // res.sendFile(path.join(__dirname + '/static/index.html'));
+        res.send("Site Builder Web App");
+    });
+}
 
 const port = process.env.PORT || 5000;
 
